ff8b3ab2f0d35832655a6f1963954b429841127e
Savvas Ashiotis <ashiots2@gmail.com>
2020-04-14 15:03:39 +0300

    finished 70 and 71 on styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
-import Radium, {StyleRoot} from "radium";
 import Person from './Person/Person';
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+      background-color: green;
+      color: white;
+      font: inherit;
+      border: 1px solid blue;
+      padding: 8px;
+      cursor: pointer;
+      
+      &:hover {
+        background-color: lightgreen;
+        color: black;
+      }`;
 
 class App extends Component {
   state = {
@@ -89,18 +102,16 @@ class App extends Component {
       classes.push("bold");
     }
 
-    return <StyleRoot>
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
-    </StyleRoot>;
+    return <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p className={classes.join(" ")}>This is really working!</p>
+      <StyledButton onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+      {persons}
+    </div>;
   }
 
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
 }
 
 // Radium is called a higher order component (component wrapping your component)
-export default Radium(App);
+export default App;
